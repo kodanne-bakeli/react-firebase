d483f6d0de1be5577acb1e4c7098d10d06ee1a06
Kodanne-bakeli <kodanne.bakeli@gmail.com>
2023-08-24 23:29:42 +0000

    revue de la fonction update

--- src/blogList.js
+++ src/blogList.js
@@ -1,10 +1,107 @@
+// import { crud,database } from "./firebase";
+// import { getFirestore,collection,
+//     onSnapshot,
+//     addDoc,
+//     deleteDoc,
+//     doc,
+//     getDoc,
+//     updateDoc, } from "firebase/firestore"
+// import { useState, useEffect } from "react";
+// const Blogs = () => {
+//     const [blogs, setBlogs] = useState([]);
+//     const [title, setTitle] = useState("");
+//     const [body, setBody] = useState("");
+//     const [newTitle, setNewTitle] = useState(title);
+//     const [newBody, setNewBody] = useState(body);
 
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         if (title !== "" && body !== "") {
+//             await crud.addTask(title, body);
+//             setTitle("");
+//             setBody("");
+//         }
+//     }
+//     const handleDelete = async (e, id) => {
+//         e.preventDefault();
+//         if (title !== "" && body !== "") {
+//             await crud.deleteTask(id);
+//             setTitle("");
+//             setBody("");
+//         }
+//     }
+//     const handleEdit = async (e) => {
+//         e.preventDefault();
+//         if (title !== "" && body !== "") {
+//             await crud.updateTask(newTitle, newBody);
+//             setNewTitle("");
+//             setNewBody("");
+//         }
+//     }
+
+
+//     const handleChange = (e) => {
+//         e.preventDefault();
+//         if (blogs.complete === true) {
+//             setNewTitle(title);
+//             setNewBody(body);
+//         } else {
+//             title = "";
+//             body = "";
+//             setNewTitle(e.target.value);
+//             setNewBody(e.target.value);
+//         }
+//     };
+//     useEffect(() => {
+//         const getBlogs = async () => {
+//             const data = await getDoc(collection(database, "todolist"));
+//             setBlogs(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+//         };
+//         getBlogs();
+//     }, []);
+//     function Form() {
+//         return (
+//             <div>
+//                 <h1>Add a New Note</h1>
+//                 <form onSubmit={handleSubmit}>
+//                     <input type="text" className="form-control w-50 mx-auto my-3" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+//                     <textarea className="form-control w-50 mx-auto" value={body} onChange={(e) => { setBody(e.target.value) }}> </textarea>
+//                     <button>Add blog</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+//     function List() {
+//         return (
+//             <div>
+//                {blogs.map((blog) => {
+//                 return (
+//                     <div>
+//                         <h1>{blog.title}</h1>
+//                         <h1>{blog.body}</h1>
+//                         <button onClick={() => {handleDelete(blog.id);}}>Delete blog</button>
+//                         <button onClick={() => {handleEdit(blog.id);}}>edit blog</button>
+//                     </div>
+//                 );
+//             })}
+//             </div>
+//         );
+//     }
+//     return (
+//         <div className="todo">
+//             <Form />
+//             <List />
+//         </div>
+//     );
+
+// }
+// export default Blogs;
 import { useState, useEffect } from "react";
 import "./App.css";
 import { database, crud } from "./firebase";
 import {
     collection,
-    updateDoc
+    updateDoc,doc
 } from "firebase/firestore";
 
 function App() {
@@ -21,7 +118,7 @@ function App() {
     const updateTitle = async (id) => {
         const blogDoc = doc(database, "todolist", id);
         await updateDoc(blogDoc, { 
-            body: newTitle});
+            title: newTitle});
     };
     const updateBody = async (id) => {
         const blogDoc = doc(database, "todolist", id);
@@ -46,22 +143,26 @@ function App() {
     function Form() {
         return(
             <div>
-<input type="text"
-                className="form-control mb-3"
-                placeholder="title..."
+                <form onSubmit={createBlog}>
+                <input type="text"
+                className="form-control mb-3 w-75 mx-auto"
+                placeholder="title... "
+                value={newTitle}
                 onChange={(event) => {
                     setNewTitle(event.target.value);
                 }}
             />
-            <textarea className="form-control"
+            <textarea className="form-control w-75 mx-auto mb-3"
                 placeholder="body..."
+                value={newBody}
                 onChange={(event) => {
                     setNewBody(event.target.value);
                 }}
             >
             </textarea>
 
-            <button className="btn btn-primary" onClick={createBlog}> Create User</button>
+            <button className="btn btn-primary" > Create User</button>
+            </form>
             </div>
         )
     }
@@ -72,7 +173,7 @@ function App() {
             {blogs.map((blog) => {
                 return (
                     <div key={blog.id}>
-                        <div className="card card-body mt-2 border-primary">
+                        <div className="card card-body mt-2 border-primary w-75 mx-auto">
                             <h3 className="card-title card-title text-success">title: {blog.title}</h3>
                             <p className="text-warning">body: {blog.body}</p>
                             <div>
